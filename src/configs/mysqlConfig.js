f6b84aa35f0f72785110519f5500b84c6fbe091b
Leonardo Piovesan <leopvsn@gmail.com>
2023-04-04 14:16:13 -0300

    refactor: removed local connection in order to use dockerized mysql container

--- src/configs/mysqlConfig.js
+++ src/configs/mysqlConfig.js
@@ -3,27 +3,11 @@ require("dotenv").config({ path: "src/configs/.env" });
 const { init } = require("../database/init");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-});
-
-connection.query(init.createDb, (error, results) => {
-  if (error) throw error;
-  connection.query(init.useDb, () => {
-    connection.query(init.createTables.manager, (error, results) => {
-      if (error) throw error;
-      connection.query(init.fillTables.manager);
-    });
-    connection.query(init.createTables.technician, (error, results) => {
-      if (error) throw error;
-      connection.query(init.fillTables.technician);
-    });
-    connection.query(init.createTables.task, (error, results) => {
-      if (error) throw error;
-      connection.query(init.fillTables.task);
-    });
-  });
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports.connection = connection;
